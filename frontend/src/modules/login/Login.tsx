d01c5bd4c28e36fdb00c7826bf19f6ce9319d5d3
IlyaVafin <rthrthrer5gh@gmail.com>
2025-11-26 17:39:18 +0300

    feat: #2 add apihandler

--- frontend/src/modules/login/Login.tsx
+++ frontend/src/modules/login/Login.tsx
@@ -4,82 +4,49 @@ import Email from "./Email"
 import Password from "./Password"
 import { Button } from "@/components/button/button"
 import { useNavigate } from "react-router-dom"
-interface ResponseError {
-	message: string
-	error: string
-	statusCode: number
-}
-interface ResponseSuccess {
-	message: string
-	success: boolean
-}
-type LoginResponse = ResponseError | ResponseSuccess
+import { useMutation } from "@tanstack/react-query"
+import { api } from "@/shared/api/ApiHandler"
+import type { LoginData } from "./types"
+
 const Login = () => {
 	const navigate = useNavigate()
 	const [email, setEmail] = useState("")
 	const [password, setPassword] = useState("")
-	const [error, setError] = useState<ResponseError>({
-		error: "",
-		message: "",
-		statusCode: 0,
-	})
 	const id = useId()
-	const handleSubmit: () => Promise<LoginResponse> = async () => {
-		try {
-			const response = await fetch("http://localhost:3000/auth/login", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				credentials: "include",
-				body: JSON.stringify({ email, password }),
-			})
-			if (!response.ok) {
-				const errorData: ResponseError = await response.json()
-				return {
-					error: errorData.error,
-					message: errorData.message,
-					statusCode: errorData.statusCode,
-				}
-			}
-			if (response.ok) {
+	const login = async () => {
+		return await api.post<LoginData, { message: string }>("/auth/login", {
+			email,
+			password,
+		})
+	}
+
+	const { isPending, isError, error, mutate } = useMutation({
+		mutationKey: ["login"],
+		mutationFn: login,
+		onSuccess: data => {
+			if (!("error" in data)) {
 				navigate("/profile")
 			}
-			const successData: ResponseSuccess = await response.json()
-			return successData
-		} catch (error: unknown) {
-			if (error instanceof Error) {
-				return {
-					error: error.name,
-					message: error.message,
-					statusCode: 0,
-				}
-			}
-			return {
-				error: "Unknown error",
-				message: "Something wrong",
-				statusCode: 500,
-			}
+		},
+		onError: (err) => {
+			console.log(err.name)
 		}
-	}
+	})
 	return (
 		<form
-			onSubmit={async (e) => {
+			onSubmit={async e => {
 				e.preventDefault()
-				const result = await handleSubmit()
-				if("error" in result) {
-					setError(result)
-				}
+				mutate()
 			}}
 			className='flex flex-col gap-8 justify-center items-center'
 		>
 			<div className='flex flex-col gap-8 max-w-[500px] w-full'>
 				<Email email={email} setEmail={setEmail} id={id} />
 				<Password id={id} password={password} setPassword={setPassword} />
-				<Button className='w-[500px] mt-4' type='submit'>
-					Login
+				<Button disabled={isPending} className='w-[500px] mt-4' type='submit'>
+					{isPending ? "Login in..." : "Login"}
 				</Button>
-				{error && <p>{error.message}</p>}
+				{isError && <p>{error.message}</p>}
 			</div>
 		</form>
 	)
--- frontend/src/shared/api/ApiHandler.ts
+++ frontend/src/shared/api/ApiHandler.ts
@@ -1,10 +1,76 @@
-interface IApiHandler {
-  BASE_URL: string 
-  get: () => void 
-  post: () => void 
-  put: () => void 
-  delete: () => void 
+interface ResponseError {
+	message: string
+	error: string
+	statusCode: number
+	success: boolean
 }
-class ApiHandler implements IApiHandler {
-  BASE_URL = 'http://localhost:3000'
+
+type ResponseFormApi<T> = ResponseError | { success: boolean; data: T }
+class ApiHandler {
+	BASE_URL = "http://localhost:3000"
+	async get<T>(url: string): Promise<ResponseFormApi<T>> {
+		try {
+			const response = await fetch(`${this.BASE_URL}${url}`, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+				credentials: "include",
+			})
+			if (!response.ok) {
+				const errorData: ResponseError = await response.json()
+				throw new Error(errorData.message)
+			}
+			const successData: T = await response.json()
+			return { success: true, data: successData }
+		} catch (error: unknown) {
+			if (error instanceof Error) {
+				throw {
+					message: error.message,
+					error: error.name,
+					statusCode: 0,
+					success: false,
+				}
+			}
+			throw {
+				message: "Someting wrong",
+				error: "Unknown error",
+				statusCode: 500,
+				success: false,
+			}
+		}
+	}
+	async post<T, K>(url: string, body: T): Promise<ResponseFormApi<K>> {
+		try {
+			const response = await fetch(`${this.BASE_URL}${url}`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				credentials: "include",
+				body: JSON.stringify(body),
+			})
+			if (!response.ok) {
+				const errorData: ResponseError = await response.json()
+        throw new Error(errorData.message)
+			}
+			const successData: K = await response.json()
+			return { success: true, data: successData }
+		} catch (error: unknown) {
+			if (error instanceof Error) {
+				throw {
+					message: error.message,
+					error: error.name,
+					statusCode: 0,
+					success: false,
+				}
+			}
+			throw {
+				message: "Someting wrong",
+				error: "Unknown error",
+				statusCode: 500,
+				success: false,
+			}
+		}
+	}
 }
+export const api = new ApiHandler()
