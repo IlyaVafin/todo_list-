e2af9b19d5f3fc8b5503742d5c24b3f715553cc7
IlyaVafin <rthrthrer5gh@gmail.com>
2025-11-27 19:20:55 +0300

    feat: #6 change register form styles

--- frontend/src/modules/register/Register.tsx
+++ frontend/src/modules/register/Register.tsx
@@ -46,54 +46,56 @@ const Register = () => {
 					password: register.password,
 				})
 			}}
-			className='flex flex-col gap-4'
+			className='flex justify-center items-center'
 		>
-			<div className='flex flex-col gap-2'>
-				<label htmlFor={`name_${id}`}>Name:</label>
-				<Input
-					onChange={e => handleChange("name", e.target.value)}
-					value={register.name}
-					id={`name_${id}`}
-					placeholder='John Doe'
-					autoComplete='username'
-				/>
-			</div>
-			<div className='flex flex-col gap-2'>
-				<label htmlFor={`email_${id}`}>Email:</label>
-				<Input
-					onChange={e => handleChange("email", e.target.value)}
-					value={register.email}
-					id={`email_${id}`}
-					placeholder='example@gmail.com'
-					autoComplete='email'
-				/>
-			</div>
-			<div className='flex flex-col gap-2'>
-				<label htmlFor={`password_${id}`}>Password</label>
-				<Input
-					onChange={e => handleChange("password", e.target.value)}
-					value={register.password}
-					type='password'
-					id={`password_${id}`}
-					placeholder='Password'
-					autoComplete='new-password'
-				/>
-			</div>
-			<div className='flex flex-col gap-2'>
-				<label htmlFor={`confirm_password_${id}`}>Confirm password</label>
-				<Input
-					onChange={e => handleChange("confirmPassword", e.target.value)}
-					value={register.confirmPassword}
-					type='password'
-					id={`confirm_password_${id}`}
-					placeholder='Confirm password'
-					autoComplete='current-password'
-				/>
-			</div>
-			<div className='flex justify-center w-full'>
-				<Button disabled={isPending}>
-					{isPending ? "Please wait..." : "Sign up"}
-				</Button>
+			<div className='flex flex-col gap-4 max-w-[350px] w-full border border-neutral-700 p-10 rounded-2xl'>
+				<div className='flex flex-col gap-2'>
+					<label htmlFor={`name_${id}`}>Name:</label>
+					<Input
+						onChange={e => handleChange("name", e.target.value)}
+						value={register.name}
+						id={`name_${id}`}
+						placeholder='John Doe'
+						autoComplete='username'
+					/>
+				</div>
+				<div className='flex flex-col gap-2'>
+					<label htmlFor={`email_${id}`}>Email:</label>
+					<Input
+						onChange={e => handleChange("email", e.target.value)}
+						value={register.email}
+						id={`email_${id}`}
+						placeholder='example@gmail.com'
+						autoComplete='email'
+					/>
+				</div>
+				<div className='flex flex-col gap-2'>
+					<label htmlFor={`password_${id}`}>Password</label>
+					<Input
+						onChange={e => handleChange("password", e.target.value)}
+						value={register.password}
+						type='password'
+						id={`password_${id}`}
+						placeholder='Password'
+						autoComplete='new-password'
+					/>
+				</div>
+				<div className='flex flex-col gap-2'>
+					<label htmlFor={`confirm_password_${id}`}>Confirm password</label>
+					<Input
+						onChange={e => handleChange("confirmPassword", e.target.value)}
+						value={register.confirmPassword}
+						type='password'
+						id={`confirm_password_${id}`}
+						placeholder='Confirm password'
+						autoComplete='current-password'
+					/>
+				</div>
+				<div className='flex justify-center w-full'>
+					<Button className='mt-2' disabled={isPending}>
+						{isPending ? "Please wait..." : "Sign up"}
+					</Button>
+				</div>
 			</div>
 		</form>
 	)
